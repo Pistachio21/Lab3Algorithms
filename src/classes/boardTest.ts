7b1fe3ddb2a2905539373eff69ab9030ee9863cd
Shawn Timothy Ike Barza <92981591+Pistachio21@users.noreply.github.com>
2024-04-09 22:22:51 +0800

    changed test cases

--- src/classes/boardTest.ts
+++ src/classes/boardTest.ts
@@ -1,12 +1,17 @@
 import Board from "./board";
-const tiles2 = [[1, 2, 3], [4, 5, 6], [7, 8, 0]];
-const board2 = new Board(tiles2);
-console.log(board2.toStrings());
-console.log(board2.dimension())
-console.log(board2.hamming());
-console.log(board2.manhattan());
-console.log(board2.isGoal());
-const neighbors = board2.neighbors();
-console.log(neighbors[0]);
-const twinBoard = board2.twin();
+const tiles = [[1, 2, 3], [4, 5, 6], [7, 8, 0]];
+const tiles2 = [[1, 3, 2], [5, 4, 6], [0, 8, 7]]
+const board2 = new Board(tiles2)
+const board = new Board(tiles);
+console.log(board.toStrings());
+console.log(board.dimension())
+console.log(board.hamming());
+console.log(board.manhattan());
+console.log(board.isGoal());
+console.log(board.equals(board2))
+const neighbors = board.neighbors();
+for (let i = 0; i < neighbors.length; i++) {
+    console.log(neighbors[i])
+}
+const twinBoard = board.twin();
 console.log(twinBoard);
